5c723869ba69c90662d9f6123a145e3588f14891
Juuro <mail@sebastian-engel.de>
2020-08-03 02:01:13 +0200

    Indentations.

--- src/components/measurement/measurement.test.js
+++ src/components/measurement/measurement.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import Measurement from './measurement'
 
 it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Measurement />, div)
-    ReactDOM.unmountComponentAtNode(div)
+  const div = document.createElement('div')
+  ReactDOM.render(<Measurement />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
--- src/index.js
+++ src/index.js
@@ -14,31 +14,31 @@ serviceWorker.register()
 let deferredPrompt
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    console.log('beforeinstallprompt')
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault()
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e
+  console.log('beforeinstallprompt')
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault()
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e
 })
 
 window.document.addEventListener('click', (event) => {
-    const element = event.target
+  const element = event.target
 
-    if (element.id === 'a2hs' && deferredPrompt) {
-        console.log('a2hs prompt')
-        // hide our user interface that shows our A2HS button
-        element.style.display = 'none'
-        // Show the prompt
-        deferredPrompt.prompt()
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice
-            .then((choiceResult) => {
-                if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the A2HS prompt')
-                } else {
-                    console.log('User dismissed the A2HS prompt')
-                }
-                deferredPrompt = null
-            })
-    }
+  if (element.id === 'a2hs' && deferredPrompt) {
+    console.log('a2hs prompt')
+    // hide our user interface that shows our A2HS button
+    element.style.display = 'none'
+    // Show the prompt
+    deferredPrompt.prompt()
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt')
+        } else {
+          console.log('User dismissed the A2HS prompt')
+        }
+        deferredPrompt = null
+      })
+  }
 })
